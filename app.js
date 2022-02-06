aaf0d0a906e15bee907c24a9f9209414d039dd59
shakilwebmaster <mdshakil102036@gmail.com>
2022-02-06 23:55:03 +0600

    Javascript pactices learn with sumith

---
+++ app.js
@@ -0,0 +1,9 @@
+let gf = "I love Mahia";
+console.log(gf);
+console.log(gf.replace('Mahia','Sanzida'));
+let gfNew = gf.replace('Mahia','Sanzida');
+console.log(gf.replace('Sanzida','FutureBow'));
+console.log(gfNew);
+if(gfNew.search('Sanzida')){
+    console.log("amar r valolagena. new bow lagbo.");
+}
